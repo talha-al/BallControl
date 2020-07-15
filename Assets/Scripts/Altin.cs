48f3f487b0245947740fe8aaedd24fd2b9e872ef
Talha Şeref Alataş <talhaalatas97@gmail.com>
2020-07-15 13:00:29 +0300

    Add

---
+++ Assets/Scripts/Altin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altin : MonoBehaviour
+{
+    [SerializeField]
+    private float dönüsHizi;
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 90) * dönüsHizi);
+
+    }
+    
+}
